6953d64862c939759723ef7cc0ca36c299ace7d2
Miles Mickelson <milesmickelson87@gmail.com>
2019-10-30 20:21:50 -0700

    project completion with all ten tests passing

--- server/app.js
+++ server/app.js
@@ -1,20 +1,60 @@
 const express = require('express');
 const fs = require('fs');
+const csv = require('csvtojson/v2');
+
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
+const logCache = [];
 
+app.use((req, res, next) => {
+  var userAgent = req.headers['user-agent'].replace(',','');
+  logCache.push(userAgent);
+
+  var time = new Date().toISOString();
+  logCache.push(time);
+
+  var method = req.method;
+  logCache.push(method);
+
+  var url = req.url;
+  logCache.push(url);
+
+  var version = 'HTTP/' + req.httpVersion;
+  logCache.push(version);
+
+  var status = res.statusCode + '\n';
+  logCache.push(status);
+
+  var logCacheJ = logCache.join(',');
+  console.log(logCacheJ);
+
+  fs.appendFile('./log.csv', logCacheJ, (err) => {
+    if (err) throw err;
+    console.log('Visitor log data has been copied to the csv file');
+  });
+
+  function emptyCache() {
+    logCache.length = 0;
+  }
+  emptyCache();
+
+  next();
 });
 
-app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+app.get('/', function( req, res) {
+  res.status(200).send('Ok');
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
+  csv()
+    .fromFile('./log.csv')
+    .then((obj)=>{
+      res.json(obj);
+  })
+});
 
+app.get('*', function( req, res){
+  res.status(404).send('Sorry, no page was found at this address');
 });
 
 module.exports = app;
